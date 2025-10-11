3f18962870395ca421c8bfab85352dbf39af30e6
Cylerss <mmuralikrishna2007@gmail.com>
2025-10-11 18:14:20 +0530

    Prototype 3

--- backend/src/lib/cloudinary.js
+++ backend/src/lib/cloudinary.js
@@ -1,6 +1,7 @@
-import {v2 as cloudinary} from 'cloudinary';
+import { v2 as cloudinary } from "cloudinary";
 
-import { config } from 'dotenv';
+import { config } from "dotenv";
+
 config();
 
 cloudinary.config({
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -55,7 +55,7 @@ const Sidebar = () => {
           >
             <div className="relative mx-auto lg:mx-0">
               <img
-                src={user.profilePic || "/avatar.png"}
+                src={user.profilePic || "https://static.vecteezy.com/system/resources/previews/019/879/186/non_2x/user-icon-on-transparent-background-free-png.png"}
                 alt={user.name}
                 className="size-12 object-cover rounded-full"
               />
